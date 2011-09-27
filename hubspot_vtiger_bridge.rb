951a28677772f1f7a3c2b55e2ef9c8091e0c0c13
Slavik Derevyanko <slavik.derevyanko@era-ehs.com>
2011-09-27 18:59:43 -0400

    Implemented incoming POST request processing. Not tested!

--- hubspot_vtiger_bridge.rb
+++ hubspot_vtiger_bridge.rb
@@ -2,10 +2,24 @@ require 'sinatra'
 require 'uri'
 require 'json'
 require 'pp'
+require File.dirname(__FILE__) + '/hubspot_webhooks_adapter'
+require File.dirname(__FILE__) + '/vtiger_crm_leads_adapter'
+require File.dirname(__FILE__) + '/vtiger_lead_custom_fields'
 
 post '/' do
-  pp request.body.string + "_END_OF_PARAMS\n"
-  hubspot_lead = JSON.parse(URI.decode(request.body.string))['1']
-  pp hubspot_lead['firstName']
-  pp hubspot_lead['lastName']
+  begin
+    hubspot_lead = JSON.parse(URI.decode(request.body.string))['1']
+    hubspot_adapter = HubspotWebhooksAdapter.new hubspot_lead
+    lead_for_vtiger = hubspot_adapter.to_vtiger_lead
+    vtiger_adapter = VTigerCRMLeadsAdapter.new
+    vtiger_adapter.login
+    lead_in_vtiger = vtiger_adapter.find_lead lead_for_vtiger[VTigerLeadCustomFields::HUBSPOT_LEAD_ID]
+    if lead_in_vtiger 
+      print "Lead found in vTiger, Hubspot id=" + lead_in_vtiger[VTigerLeadCustomFields::HUBSPOT_LEAD_ID]
+    else
+      vtiger_adapter.create_lead lead_for_vtiger
+    end
+  rescue Exception => error
+    pp error
+  end
 end
